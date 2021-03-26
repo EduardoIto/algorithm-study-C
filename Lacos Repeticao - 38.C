8d538d3de74032b9b259d862613c4271a9ce94d7
Eduardo Ito <73322050+EduardoIto@users.noreply.github.com>
2021-03-26 11:15:27 -0300

    Add files via upload

---
+++ Lacos Repeticao - 38.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int numero = 6;
+
+
+    while(numero <= 11){
+        printf(" %d ", numero);
+        numero++;
+    }
+
+     printf("Acabou!\n");
+
+    return 0;
+}
