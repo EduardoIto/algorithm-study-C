788fc5cf4fdff6dc153a2d6dc5f7a17ba0fb51e3
Eduardo Ito <73322050+EduardoIto@users.noreply.github.com>
2021-04-16 16:22:54 -0300

    Add files via upload

---
+++ Funcoes - 86.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+void gerador(){
+    printf("        OLA, MUNDO!\n");
+}
+
+int main()
+{
+    printf("+-------===========--------+\n");
+    gerador();
+    printf("+-------===========--------+\n");
+
+    return 0;
+}
